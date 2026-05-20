45206023c96a6c71e53112acc48cbc48e10dbcb6
lucasGabrielCortez <lucas.cortez@escola.pr.gov.br>
2026-05-20 12:36:23 +0000

    p

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-def main():
-    print("Hello, World!")
+# import pprint
+# nomes = ["Ana", "Bruno", "Carla", "Diego", "Fernanda"]
+# print(nomes[1])
+# print(len(nomes))
+# nomes.append("Gabriel")
+# nomes.insert(1, "Allan")
+# print(nomes)
+# nomes.clear()
+# print(nomes)
+# del(nomes)
+# print(nomes)
 
-if __name__ == "__main__":
-    main()
+# Exemplo de Dicionário
+info = {
+  "Nomes": ["Ana", "Bruno", "Carla"]
+  "Idades":(10, 25, 14),
+  "Parentesco":{
+    "Nome": "Ana",
+    "Pais": ["Maria" , "João"]
+
+  }
+}
+
+# print(info.Nomes)                             
+
+
+#Exemplo de Tupla
+notas = (9.4, 8.3, 5.2, 10,0, 4.8)
+print(f"Nota na posição 2: {notas[2]}")
+notas.insert(1, 10.0)
+notas.append(5.8)
+del(notas)
