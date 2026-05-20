4c905fe155c5741323dd4e296bbc971670cca852
lucasGabrielCortez <lucas.cortez@escola.pr.gov.br>
2026-05-20 13:22:34 +0000

    fvddgn

--- main.py
+++ main.py
@@ -21,12 +21,20 @@ info = {
   }
 }
 
+print(info)
+print(info["Nomes"])
+#print(info["Telefones"])
+print(info.get("Telefones"))
+for chave, valor in info.items():
+  print(f"{chave} : {valor}")
+
+
 # print(info.Nomes)                             
 
 
 #Exemplo de Tupla
-notas = (9.4, 8.3, 5.2, 10,0, 4.8)
-print(f"Nota na posição 2: {notas[2]}")
-notas.insert(1, 10.0)
-notas.append(5.8)
-del(notas)
+# notas = (9.4, 8.3, 5.2, 10,0, 4.8)
+# print(f"Nota na posição 2: {notas[2]}")
+# notas.insert(1, 10.0)
+# notas.append(5.8)
+# del(notas)
